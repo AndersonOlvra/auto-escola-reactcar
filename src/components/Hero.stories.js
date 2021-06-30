8b0ed2a4d660f2eb56b98920716b3f5cd7ca1f74
AndersonOlvra <andersonarturoliveira@gmail.com>
2021-06-30 20:59:39 -0300

    add heading storybook

---
+++ src/components/Hero.stories.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import Hero from "./Hero";
+import BgHighwayImage from "../stories/assets/traffic.jpg";
+import BgCarImage from "../stories/assets/porsche.jpg";
+import Heading from "./Heading";
+
+export default {
+  title: "Hero",
+  component: Hero,
+};
+
+export const usage = () => (
+  <Hero image={BgHighwayImage}>
+    <h1>Ganhe sua liberdade para ir e vir</h1>
+    <p>A auto escola lider em aprovação.</p>
+  </Hero>
+);
+
+export const withList = () => (
+  <Hero image={BgCarImage}>
+    <Heading>
+      <h1>
+        Ganhe sua <strong>liberdade</strong>
+        <br />
+        para ir e vir
+      </h1>
+    </Heading>
+    <ul>
+      <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+      <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+      <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+    </ul>
+  </Hero>
+);
